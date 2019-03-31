3bd608af2633d37faf8588a238492eff90886232
Luca Giovagnoli <luca@yelp.com>
2019-03-31 12:33:35 -0700

    Adding HPT, XGBoost, Gradient Boosting and Cross Validation

---
+++ titanic/hyperparameter_tuning.py
@@ -0,0 +1,63 @@
+import pandas
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBClassifier
+
+from model import preproc
+
+def hyperparameter_tuning(
+    classifier,
+    param_grid,
+    X_train,
+    y_train,
+):
+
+    grid_search = GridSearchCV(
+        estimator=classifier,
+        param_grid=param_grid,
+        n_jobs=6,
+        cv=5,
+    )
+
+    grid_search.fit(X_train, y_train)
+    print("Best score: " + str(grid_search.best_score_))
+    print("Best params: " + str(grid_search.best_params_))
+
+    return grid_search
+
+
+def hpt_gradient_boosting(X_train, y_train):
+    """
+    https://machinelearningmastery.com/configure-gradient-boosting-algorithm/
+    """
+    param_grid = {
+        'learning_rate':[0.15,0.1,0.05,0.01],
+        'n_estimators':[100,250,500,750,1000],
+        'max_depth': list(range(3, 7)),
+    }
+
+    classifier = GradientBoostingClassifier()
+    return hyperparameter_tuning(classifier, param_grid, X_train, y_train)
+
+
+def hpt_xgboost(X_train, y_train):
+    """  """
+    param_grid = {
+        'learning_rate':[0.15,0.1,0.05,0.01],
+        'n_estimators':[100,250,500,750,1000],
+        'max_depth': list(range(3, 7)),
+    }
+
+    classifier = XGBClassifier()
+    return hyperparameter_tuning(classifier, param_grid, X_train, y_train)
+
+
+if __name__ == '__main__':
+    dataset = pandas.read_csv('train.csv')
+    targets = dataset['Survived']
+
+    train_set = preproc(dataset.drop('Survived', axis=1))
+
+    # tuning_result = hpt_gradient_boosting()
+    tuning_result = hpt_xgboost(train_set, targets)
--- titanic/model.py
+++ titanic/model.py
@@ -1,3 +1,41 @@
+"""
+Some personal notes
+========================
+
+
+'Sex' seems to be the best predictor for survival on the titanic.
+
+**Features correlation**
+========================
+
+Seems that First class correlates to:
+- fare (obviously)
+- sex (males tend to be in first class -> be richer?)
+- Age (older people tend to be in better classes)
+
+          Pclass       Sex       Age     SibSp     Parch      Fare
+Pclass  1.000000  0.131900 -0.331339  0.083081  0.018443 -0.549500
+Sex     0.131900  1.000000  0.084153 -0.114631 -0.245489 -0.182333
+Age    -0.331339  0.084153  1.000000 -0.232625 -0.179191  0.091566
+SibSp   0.083081 -0.114631 -0.232625  1.000000  0.414838  0.159651
+Parch   0.018443 -0.245489 -0.179191  0.414838  1.000000  0.216225
+Fare   -0.549500 -0.182333  0.091566  0.159651  0.216225  1.000000
+
+
+**HPT**
+========================
+
+Input:
+    'learning_rate':[0.15,0.1,0.05,0.01,0.005,0.001],
+    'n_estimators':[100,250,500,750,1000,1250,1500,1750],
+    'max_depth': list(range(3, 10)),
+
+Best params: {'learning_rate': 0.05, 'max_depth': 3, 'n_estimators': 500}
+
+
+"""
+
+
 import math
 import matplotlib.pyplot as plt
 import numpy
@@ -6,78 +44,146 @@ import pydotplus
 
 from sklearn import tree
 from sklearn import preprocessing
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBRegressor
+from xgboost import XGBClassifier
+from sklearn.base import clone
 
 
-def load_csv_to_numpy(filename):
-    dataset = pandas.read_csv(filename)
-    dataset = dataset.fillna(0)
-
-    # from numpy import genfromtxt
-    # my_data = genfromtxt(
-        # filename,
-        # delimiter=',',
-        # names=True,
-        # missing_values='',
-        # filling_values=0.0,
-    # )
-
-    npy_data = numpy.copy(dataset.values)
-    columns_labels = dataset.columns.values
-
-    return npy_data, columns_labels
+columns = ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+COLUMNS_TO_DROP = ['Name', 'Ticket', 'Cabin', 'Embarked']
+CROSS_VALIDATION_K_FOLDS = 5
 
 
-def preproc(matrix, columns_to_encode):
-    X = numpy.copy(matrix)
+def write_solution(trained_model):
+    dataset = pandas.read_csv('test.csv')
+    test_set = preproc(dataset)
+
+    predictions = trained_model.predict(test_set)
+
+    result = pandas.DataFrame(predictions, columns=['Survived'])
+    result['PassengerId'] = dataset.index
+    result.set_index('PassengerId', inplace=True)
+
+    result.to_csv('my_answer.csv')
+
+
+def preproc(dataset):
+    """ Data preprocessing """
+
+    # Not sure what to do with PassengerId and Names for now
+    dataset.set_index('PassengerId', inplace=True)
+    dataset.drop(COLUMNS_TO_DROP, axis=1, inplace=True)
+
+    dataset['Age'].fillna(dataset['Age'].mean(), inplace=True)
+    dataset['Fare'].fillna(dataset['Fare'].mean(), inplace=True)
+
+    # Sex to {0,1}
+    label_binarizer = preprocessing.LabelBinarizer().fit(dataset['Sex'])
+    dataset['Sex'] = label_binarizer.transform(dataset['Sex'])
+
+    return dataset
+
+
+def _scores_mean_accuracy(scores, clf_name):
+    return f"[{clf_name}] CV Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+
+
+def cross_validation(classifier, train_set, targets):
+    clf_name=classifier.__class__
+
+    # Cross validation
+    scores = cross_val_score(classifier, train_set, targets, cv=CROSS_VALIDATION_K_FOLDS)
+    print(_scores_mean_accuracy(scores, clf_name))
+
+
+def train_decision_tree(train_set, targets):
+    classifier = tree.DecisionTreeClassifier()
     
-    for column_to_encode in columns_to_encode:
-	label_encoder = preprocessing.LabelEncoder()
-	label_encoder.fit(X[:,column_to_encode])
-	encoded_labels = label_encoder.transform(X[:,column_to_encode])
-	X[:, column_to_encode] = encoded_labels
+    trained_model = cross_validation(classifier, train_set, targets)
 
-    return X
+    # Generate graphical representation of a decision tree
+    dot_data = tree.export_graphviz(trained_model, out_file=None, feature_names=train_set.columns)
+    graph = pydotplus.graph_from_dot_data(dot_data)
+    graph.write_pdf("decision_tree.pdf")
 
-
-def train_model(npy_data):
-
-    Y = npy_data[:,1].astype(int)
-    Y.reshape(1, len(Y))
-    X = npy_data[:,2:]
-
-    X = preproc(X, [1, 2, 6, 8, 9])
-
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(X, Y)
-
-    return clf
-
-def load_results():
-    passenger_to_survival = {}
-    npy_data, _ = load_csv_to_numpy("gender_submission.csv")
-
-    for record in npy_data:
-        passenger_to_survival[record[0]] = record[1]
-
-    return passenger_to_survival
-
-npy_data, column_labels = load_csv_to_numpy("train.csv")
-clf = train_model(npy_data)
-
-npy_test, _ = load_csv_to_numpy("test.csv")
-X_test = preproc(npy_test, [2, 3, 7, 9, 10])
+    return trained_model
 
 
-passenger_to_survival = load_results()
+def train_model(train_set, targets):
 
-count = 0
-for record in X_test:
-    if clf.predict(record.reshape(1, len(record))[:, 1:]) == passenger_to_survival[record[0]]:
-        count += 1
+    classifier = GradientBoostingClassifier(
+        learning_rate=0.05,
+        max_depth=3,
+        n_estimators=500,
+    )
 
-print(float(count)/float(len(passenger_to_survival)))
+    cross_validation(classifier, train_set, targets)
 
-dot_data = tree.export_graphviz(clf, out_file=None, feature_names=column_labels[1:])
-graph = pydotplus.graph_from_dot_data(dot_data)
-graph.write_pdf("iris.pdf")
+    # AUROC score computation
+    # scores = trained_model.predict_proba(train_set)[:, 1]
+    # fpr, tpr, thresholds = roc_curve(targets, scores)
+    # auroc_score = roc_auc_score(targets, scores)
+    # print("AUROC score of the model: " + str(auroc_score))
 
+
+    # cross_validation clones models already but clone again for readability
+    final_model = clone(classifier).fit(train_set, targets)
+
+    return final_model
+
+
+def XGBoost_training(train_set, targets):
+    """
+    Manual Cross Validation
+    """
+    kfold = KFold(n_splits=5)
+
+    classifier = XGBClassifier(
+        n_estimators=500,
+        learning_rate=0.05,
+    )
+
+    cv_scores = []
+    for train_fold_ixs, test_fold_ixs in kfold.split(train_set):
+
+        trained_model = clone(classifier).fit(
+            train_set.iloc[train_fold_ixs],
+            targets.iloc[train_fold_ixs],
+            # early_stopping_rounds=5,
+            # evaluate `stopping` over the left out test Fold
+            # eval_set=[train_set.iloc[test_fold_ixs], targets.iloc[test_fold_ixs]],
+        ) 
+        predictions = trained_model.predict(train_set.iloc[test_fold_ixs])
+        cv_scores.append(
+            trained_model.score(
+                train_set.iloc[test_fold_ixs],
+                targets.iloc[test_fold_ixs],
+            )
+        )
+
+    cv_scores = numpy.array(cv_scores)
+    print(_scores_mean_accuracy(cv_scores, 'XGBoost'))
+
+    # clone again for readability
+    final_model = clone(classifier).fit(train_set, targets)
+
+    return final_model
+
+
+if __name__ == '__main__':
+    dataset = pandas.read_csv('train.csv')
+    targets = dataset['Survived']
+
+    train_set = preproc(dataset.drop('Survived', axis=1))
+
+    trained_gradien_boosting_model = train_model(train_set, targets)
+    trained_xgboost_model = XGBoost_training(train_set, targets)
+
+    write_solution(trained_xgboost_model)
